2a8559f3536a7cb69c84b0bc088216361c5b8607
“Tyler <tylerpalef@gmail.com>
2018-08-31 12:51:37 -0400

    finished assignment

--- temperature.rb
+++ temperature.rb
@@ -1,10 +1,25 @@
 class Temperature
-  def initialize(x)
-  end
 
+  def initialize(temp = {})
+    @celsius = temp[:c]
+    @fahrenheit = temp[:f]
+  end
+  
   def to_celsius
+    if @celsius
+      return @celsius
+    else
+      return @celsius ||= (@fahrenheit - 32) * 5.0 / 9.0
+    end
+
   end
 
   def to_fahrenheit
-  end 
+    if @fahrenheit
+      return @fahrenheit
+    else
+      return @fahrenheit ||= (@celsius * (9.0/5.0)) + 32
+    end
+  end
+
 end
